6816e24b2579029655fa01a10991ed4a953eb666
Kachang31 <faizal.urratori@gmail.com>
2021-09-12 18:30:47 +0700

    Update Resource

--- src/routes/jenis.ts
+++ src/routes/jenis.ts
@@ -3,15 +3,49 @@ const router = express.Router();
 import Db from '../libs/db';
 
 /* GET home page. */
-router.get('/tampil', async function(req: Request, res: Response, next: NextFunction) {
-    try {
-      const d = await Db.query('SELECT * FROM jenis_kaca');
-      res.json(d);
-    } catch(err) {
-      console.log(err);
-    } finally {
-      res.json({ message: process.env.DB_USER, status: true, timeStamp: 324234243 })
-    }
-  });
+router.get('/', async function(req: Request, res: Response, next: NextFunction) {
+  try {
+    const d = await Db.query('SELECT * FROM jenis_kaca');
+    res.json(d);
+  } catch(err) {
+    console.log(err);
+  } finally {
+    res.json({ message: 'Oke' })
+  }
+});
+
+router.post('/', async function(req: Request, res: Response) {
+  const input = req.body;
+  try {
+    await Db.query('INSERT INTO jenis_kaca VALUES(NULL, ?, ?, ?, ?)', [input.nama, input.panjang, input.lebar, input.tebal])
+  } catch(err) {
+    console.log(err);
+  } finally {
+    res.json({ message: 'oke' });
+  }
+});
+
+router.put('/:id', async function(req: Request, res: Response) {
+  const input = req.body;
+  const id = req.params.id;
+  try {
+    await Db.query('UPDATE jenis_kaca SET nama = ?, panjang = ?, lebar = ?, tebal = ? WHERE id = ?', [input.nama, input.panjang, input.lebar, input.tebal, id])
+  } catch(err) {
+    console.log(err);
+  } finally {
+    res.json({ message: 'oke' });
+  }
+});
+
+router.delete('/:id', async function(req: Request, res: Response) {
+  const id = req.params.id;
+  try {
+    await Db.query('DELETE FROM jenis_kaca WHERE id = ?', [id])
+  } catch(err) {
+    console.log(err);
+  } finally {
+    res.json({ message: 'oke' });
+  }
+});
 
   export default router;
--- src/routes/stok.ts
+++ src/routes/stok.ts
@@ -3,14 +3,46 @@ const router = express.Router();
 import Db from '../libs/db';
 
 /* GET home page. */
-router.get('/tampil', async function(req: Request, res: Response, next: NextFunction) {
+router.get('/', async function(req: Request, res: Response, next: NextFunction) {
     try {
       const d = await Db.query('SELECT * FROM stok_kaca');
       res.json(d);
     } catch(err) {
       console.log(err);
+    }
+  });
+  
+  router.post('/', async function(req: Request, res: Response) {
+    const input = req.body;
+    try {
+      await Db.query('INSERT INTO stok_kaca VALUES(NULL, ?, ?, ?, ?)', [input.idJenis, input.tanggal, input.stok, input.harga])
+    } catch(err) {
+      console.log(err);
     } finally {
-      res.json({ message: process.env.DB_USER, status: true, timeStamp: 324234243 })
+      res.json({ message: 'oke' });
+    }
+  });
+  
+  router.put('/:id', async function(req: Request, res: Response) {
+    const input = req.body;
+    const id = req.params.id;
+    try {
+      await Db.query('UPDATE stok_kaca SET id_jenis_kaca = ?, stok = ?, harga = ? WHERE id = ?', [input.idJenis, input.stok, input.harga, id])
+    } catch(err) {
+      console.log(err);
+    } finally {
+      res.json({ message: 'oke' });
+    }
+  });
+  
+  router.delete('/:id', async function(req: Request, res: Response) {
+    const id = req.params.id;
+    try {
+      await Db.query('DELETE FROM stok_kaca WHERE id = ?', [id])
+    } catch(err) {
+      console.log(err);
+    } finally {
+      res.json({ message: 'oke' });
     }
   });
 
