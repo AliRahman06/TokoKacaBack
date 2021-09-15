69ce5246243f339426baada43f9cba28c46398ed
Kachang31 <faizal.urratori@gmail.com>
2021-09-15 21:37:27 +0700

    Update SRC

--- src/routes/tampil.ts
+++ src/routes/tampil.ts
@@ -2,15 +2,6 @@ import express, { NextFunction, Request, Response } from 'express';
 const router = express.Router();
 import Db from '../libs/db';
 
-/* GET home page. */
-    router.get('/stok', async function(req: Request, res: Response, next: NextFunction) {
-    try {
-        const d = await Db.query('SELECT stok_kaca.*, jenis_kaca.nama, jenis_kaca.panjang, jenis_kaca.lebar, jenis_kaca.tebal FROM stok_kaca LEFT JOIN jenis_kaca ON stok_kaca.id_jenis_kaca = jenis_kaca.id');
-        res.json(d);
-    } catch(err) {
-        console.log(err);
-    }
-    });
 
     router.get('/stok/history/:id', async function(req: Request, res: Response, next: NextFunction) {
     const id = req.params.id;
@@ -52,17 +43,7 @@ import Db from '../libs/db';
     router.get('/subtransaksi/:id', async function(req: Request, res: Response, next: NextFunction) {
     const id = req.params.id;
     try {
-        const d = await Db.query('SELECT detil_transaksi.*, transaksi.tanggal, transaksi.total, transaksi.bayar, transaksi.kembali, jenis_kaca.nama FROM detil_transaksi LEFT JOIN transaksi ON detil_transaksi.id_transaksi = transaksi.id LEFT JOIN jenis_kaca ON detil_transaksi.id_jenis_kaca= jenis_kaca.id WHERE id_transaksi = ?', [id]);
-        res.json(d);
-    } catch(err) {
-        console.log(err);
-    }
-    });
-
-    router.get('/subpembeli/:id', async function(req: Request, res: Response, next: NextFunction) {
-    const id = req.params.id;
-    try {
-        const d = await Db.query('SELECT * FROM pembeli WHERE id = ?', [id]);
+        const d = await Db.query('SELECT detil_transaksi.*, transaksi.tanggal, transaksi.total, transaksi.bayar, transaksi.kembali, jenis_kaca.nama AS nama_kaca FROM detil_transaksi LEFT JOIN transaksi ON detil_transaksi.id_transaksi = transaksi.id LEFT JOIN jenis_kaca ON detil_transaksi.id_jenis_kaca= jenis_kaca.id WHERE id_transaksi = ?', [id]);
         res.json(d);
     } catch(err) {
         console.log(err);
@@ -70,15 +51,12 @@ import Db from '../libs/db';
     });
     
     router.get('/stok', async function(req: Request, res: Response, next: NextFunction) {
-    const id = req.params.id;
     try {
-        const d = await Db.query('SELECT stok_kaca.*, jenis_kaca.nama, jenis_kaca.panjang, jenis_kaca.lebar, jenis_kaca.tebal FROM stok_kaca FULL JOIN jenis_kaca ON stok_kaca.id_jenis_kaca = jenis_kaca.id');
+        const d = await Db.query('SELECT * FROM jenis_kaca','SELECT stok FROM stok_kaca');
         res.json(d);
     } catch(err) {
         console.log(err);
     }
     });
-    
-    
 
 export default router;
